b8ebe60ea53603dfaf70361e95e41ab846d97bd8
cc <cc2016478006@outlook.com>
2022-04-02 13:35:04 +0800

    while + if

---
+++ test2.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+
+	int line = 0;
+	printf("努力敲代码%d\n");
+
+	while(line<20000)
+	{	
+		printf("敲一行代码%d\n", line);
+		line++;
+
+
+	
+}
+	if (line >= 20000)
+		printf("你会有好offer%d\n",line);
+	return 0;
+
+
+
+
+
+
+}
+
